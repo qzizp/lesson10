52304dd9c092b77a26b16c667b346433f83233a7
Nikolay Migunov <truenikolas@gmail.com>
2017-09-22 22:11:28 +0300

    добавил код в классы, которые наследуются от своих супер-классов

--- index.php
+++ index.php
@@ -8,37 +8,147 @@
     abstract public function makeBeepSignal();
   }
 
+  class Car extends GroundTransport
+  {
+
+    public $numberOfWheels;
+
+    public function makeBeepSignal()
+    {
+      // TODO: Implement makeBeepSignal() method.
+      echo "Beep-beee! <br>" ;
+    }
+
+    public function makeWheels()
+    {
+        if ($numberOfWheels === 4) {
+            echo "Я машина. У меня вот столько колёс: {$this->numberOfWheels}. ";
+        } else {
+            echo "Я странная машина. У меня вот столько колёс: {$this->numberOfWheels}. ";
+        }
+    }
+  }
+
+  $tesla = new Car();
+  $tesla->numberOfWheels = rand(1,10);
+  $tesla->makeWheels();
+  $tesla->makeBeepSignal();
+
   // TV
   abstract class HomeElectronics
   {
-    public $voltage;
-
-    abstract public function turnOn();
+    abstract public function turnOn($on);
   }
+
+  class TV extends HomeElectronics
+  {
+      public function turnOn($on)
+      {
+        // TODO: Implement turnOn() method.
+        if ($on === 1) {
+            echo "Я телевизор. Я включен.";
+        } else {
+            echo "Я телевизор. Отстань, я отдыхаю.";
+          }
+      }
+  }
+
+  $samsung = new TV();
+  $samsung->turnOn(rand(0,1));
 
   // Pen
   abstract class DrawingSubjects
   {
     public $brand;
+    public $colorOfInk;
 
     abstract public function draw();
   }
+
+  class Pen extends DrawingSubjects
+  {
+    public $brand;
+    public $colorOfInk;
+
+    public function __construct($brand, $colorOfInk)
+    {
+      $this->brand = $brand;
+      $this->colorOfInk = $colorOfInk;
+    }
+
+    public function draw()
+    {
+      // TODO: Implement draw() method.
+      echo "<br>Я ручка от {$this->brand}. Сегодня будем рисовать таким цветом: {$this->colorOfInk}";
+    }
+  }
+
+  $firstPen = new Pen("BIC", "Зелёный");
+  $secondPen = new Pen("SHMIK", "Красный");
+  $firstPen->draw();
+  $secondPen->draw();
 
   // Duck
   abstract class Birds
   {
     public $wingspan;
 
-    abstract public function fly();
+    abstract public function fly($wingspan);
   }
+
+  class Duck extends Birds
+  {
+    public $wingspan;
+
+    public function __construct($wingspan)
+    {
+      $this->wingspan = $wingspan;
+    }
+
+    public function fly($wingspan)
+      {
+        // TODO: Implement fly() method.
+        if ($wingspan < 0.05) {
+            echo "<br> Вы чё?! Я не умею летать! У меня размах крыльев всего {$this->wingspan} м";
+        } else {
+            echo "<br> Полетели!";
+        }
+      }
+  }
+
+  $yellowDuck = new Duck(rand(1,100)/100);
+  $yellowDuck->fly(wingspan);
 
   // Product
   abstract class Goods
   {
+    public $name;
     public $price;
 
-    abstract public function getPrice();
+    abstract public function getPrice($sale);
   }        
+
+  class Product extends Goods
+  {
+    public $name;
+    public $price;
+
+    public function __construct($name, $price)
+    {
+      $this->name = $name;
+      $this->price = $price;
+    }
+
+    public function getPrice($sale)
+    {
+      // TODO: Implement getPrice() method.
+      $this->price = $this->price - ($this->price * $sale);
+      return $this->price;
+    }
+  }
+
+  $toy = new Product("Медевжонок", 1200);
+  echo "<hr>" . "Товар: " . $toy->name . "<br>" . "Цена: " . $toy->getPrice(0.17);
 
  ?>
 
@@ -61,6 +171,9 @@
     <div class="interface-and-abstract">
       <h2>Отличие интерфейсов и абстрактных классов</h2> 
       <p>Абстрактный класс может иметь свойства и методы, а интерфейс только методы (без реализации). Интерфейс носит чисто описательный характер. Кроме этого, наследник интерфейса может реализовывать методы не только этого интерфейса, но и сколь угодно других, а у асбтрактного класса наследник имеет только одного родителя. Если нужно просто описать как будет работать наследник, то следует использовать интерфейс.</p>
+    </div>
+    <div>
+      <a href="https://github.com/qzizp/lesson10/blob/master/index.php">Суперклассы здесь</a>
     </div>    
   </div>
 </body>
